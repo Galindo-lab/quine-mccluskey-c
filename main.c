3e914066252bcab92869ce43d671c0c26a59da9b
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-04-15 20:03:56 +0000

    Documentacion agregada

--- main.c
+++ main.c
@@ -2,22 +2,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char getBitState(int value, char position) {
-    return value & (1<<position);
+/**
+ * Tipo de parametro que se para a las funciones
+ * limita la cantidad maxima de variables que puede usar el programa
+ */
+#define PARAMETER_TYPE char
+
+/**
+ * Numero maximo de variables en el programa.
+ * esta limitado por la longitud en bites del PARAMETER_TYPE
+ */
+#define MAX_VARS 8
+
+/**
+ * @param value numero de interes 
+ * @param position posicion 
+ * @return estado del bit en la posicion
+*/
+char BitState(PARAMETER_TYPE value, char position)
+{
+    return (value & (1 << position)) ? 1 : 0;
 }
 
-void printBinary(int value)
+/**
+ * @return numero de diferencias 
+*/
+char BitDiff(PARAMETER_TYPE a, PARAMETER_TYPE b)
 {
-    unsigned char lenght = sizeof(int) * 8 - 1;
+    int mask = a ^ b; // diferencias '1' valore iguales '0'
+    int differences = 0;
 
-    for (char i = lenght; i >= 0; i--)
-        printf(getBitState(value, i) ? "1" : "-");
+    for (char i = MAX_VARS - 1; i >= 0; i--)
+        differences += BitState(mask, i);
+
+    return differences;
+}
+
+/**
+ * Coloca la representacion de value en stdout}
+ * @param value 
+*/
+void BitPrint(PARAMETER_TYPE value)
+{
+    for (char i = MAX_VARS - 1; i >= 0; i--)
+        printf(BitState(value, i) ? "1" : "_");
 }
 
 int main()
 {
-    int a = 0b00000010000101;
-    printBinary(a);
+    char a = 0b00001010;
+    char b = 0b00000011;
+
+    BitPrint(a);
+    puts("");
+    BitPrint(b);
+    puts("");
+
+    printf("diferencias: %d\n", BitDiff(a, b));
 
     return EXIT_SUCCESS;
 }
