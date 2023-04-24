821770fdbe2e996337cd04f02ecb81cf43f3d333
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-04-24 00:45:33 +0000

    ya funciona

---
+++ import/list.h
@@ -0,0 +1,164 @@
+/**
+ * AUTOR: Luis Eduardo Galindo Amaya                           FECHA: 23-04-2023
+ *
+ * DESCRIPCIÓN:
+ * En este archivo se almacenan todos los métodos para controlar una lista
+ * enlazada.
+ */
+
+#ifndef linkedList
+#define linkedList
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+/**
+ * Obtener el valor en una posicion y converitirlo a su tipo
+ */
+#define LIST_GET(TIPE, LIST, INDEX) (*((TIPE *)(ListGetNode(LIST, INDEX)->data)))
+
+/**
+ * Nodo de la lista
+ */
+typedef struct Node
+{
+    void *data;
+    struct Node *next;
+} Node;
+
+/**
+ * Lista que contiene a los nodos
+ */
+typedef struct List
+{
+    Node *data;
+    int lenght;
+} List;
+
+/**
+ * Crear un nodo para la lista
+ * @param puntero a cosa random
+ */
+Node *NodeCreate(void *data)
+{
+    Node *foo = (Node *)malloc(sizeof(Node));
+    // if (foo == NULL)
+    //     // Fallo al alocar memoria
+    //     return NULL;
+
+    foo->data = data;
+    foo->next = NULL;
+    return foo;
+}
+
+/**
+ * liberar el nodo
+ */
+void NodeFree(Node *node)
+{
+    free(node->data);
+    node->next = NULL;
+}
+
+/**
+ * Validar si la lista esta vacia
+ */
+int ListEmpty(List *list)
+{
+    return list->data == NULL;
+}
+
+/**
+ * Cantidad de nodos en la lista
+ */
+int ListLenght(List *list)
+{
+    return list->lenght;
+}
+
+/**
+ * Inicializar lista
+ */
+List ListInit(List *list)
+{
+    list->data = NULL;
+    list->lenght = 0;
+    return *list;
+}
+
+/**
+ * Obtener el puntero a un nodo de la lista
+ */
+Node *ListGetNode(List *list, int index)
+{
+    assert(index >= 0);
+    assert(index <= ListLenght(list));
+
+    Node *node = list->data;
+    for (int i = 0; i < index; i++)
+    {
+        node = node->next;
+    }
+
+    return node;
+}
+
+/**
+ * Insertar un elemento en la lista
+ */
+void ListInsert(List *list, int index, void *data)
+{
+    assert(index >= 0);
+    assert(index <= ListLenght(list));
+
+    Node *newNode = NodeCreate(data);
+
+    if (ListEmpty(list) || index == 0)
+    {
+        newNode->next = list->data;
+        list->data = newNode;
+        list->lenght++;
+        return;
+    }
+
+    // obtiene la posicion anterior
+    Node *prev = ListGetNode(list, index - 1);
+    Node *curret = prev->next;
+
+    newNode->next = curret;
+    prev->next = newNode;
+    list->lenght++;
+}
+
+void ListRemove(List *list, int index)
+{
+    assert(index >= 0);
+    assert(index <= ListLenght(list));
+    assert(!ListEmpty(list));
+
+    if (index == 0)
+    {
+        Node *next = (list->data)->next;
+        free(list->data);
+        list->data = next;
+        list->lenght -= 1;
+        return;
+    }
+
+    // obtiene la posicion anterior
+    Node *prev = ListGetNode(list, index - 1);
+    Node *curret = prev->next;
+
+    prev->next = curret->next;
+    free(curret);
+    list->lenght -= 1;
+}
+
+void ListFree(List *list) 
+{
+    while (!ListEmpty(list))
+        ListRemove(list,0);
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,133 +1,36 @@
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
 
-typedef struct Node
+#include "./import/list.h"
+
+
+int *createInt(int a)
 {
-    void *data;
-    struct Node *next;
-} Node;
-
-Node *NodeCreate(void *data)
-{
-    Node *foo = malloc(sizeof(Node));
-    if (foo == NULL)
-        // Fallo al alocar memoria
-        return NULL;
-
-    foo->data = data;
-    foo->next = NULL;
-    return foo;
+    int *c = malloc(sizeof(int));
+    *c = a;
+    return c;
 }
 
-void NodeFree(Node *node)
-{
-    free(node->data);
-    node->next = NULL;
-}
 
-typedef struct List
-{
-    Node *data;
-    int lenght;
-} List;
-
-int ListLenght(List *list)
-{
-    return list->lenght;
-}
-
-/**
- * Validaciones comunes entre los metodos
- */
-int ListValidIndex(List *list, int index)
-{
-    if (index < 0)
-    {
-        puts("idex < 0");
-        return 0;
-    }
-
-    if (index > ListLenght(list))
-    {
-        puts("index > ListLenght");
-        return 0;
-    }
-
-    return 1;
-}
-
-List ListInit(List *list)
-{
-    list->data = NULL;
-    list->lenght = 0;
-    return *list;
-}
-
-Node *ListGetNode(List *list, int index)
-{
-    assert(ListValidIndex(list, index));
-
-    Node *node = list->data;
-    for (int i = 0; i < index; i++)
-    {
-        node = node->next;
-    }
-
-    return node;
-}
-
-void ListInsert(List *list, int index, void *data)
-{
-    assert(ListValidIndex(list, index));
-    Node *new = NodeCreate(data);
-
-    if (ListLenght(list) == 0 || index == 0)
-    {
-        new->next = list->data;
-        list->data = new;
-        list->lenght++;
-        return;
-    }
-    
-    // obtiene la posicion anterior
-    Node *prev = ListGetNode(list, index - 1);
-    Node *curret = prev->next;
-
-    new->next = curret;
-    prev->next = new;
-    list->lenght++;
-}
 
 int main()
 {
     List list = ListInit(&list);
-    int *c = malloc(sizeof(int));
-    *c = 2;
 
-    ListInsert(&list, 0, c);
+    ListInsert(&list, 0, createInt(0));
+    ListRemove(&list, 0);
 
-    c = malloc(sizeof(int));
-    *c = 1;
+    ListInsert(&list, 0, createInt(1));
+    ListInsert(&list, 1, createInt(2));
+    ListInsert(&list, 0, createInt(3));
+    ListRemove(&list, 0);
 
-    ListInsert(&list, 0, c);
+    // for (int i = 0; i < ListLenght(&list); i++)
+    // {
+    //     // int a = *((int *)(ListGetNode(&list, i)->data));
+    //     int a = LIST_GET(int, &list, i);
+    //     printf("%d\n", a);
+    // }
 
-    c = malloc(sizeof(int));
-    *c = 0;
-
-    ListInsert(&list, 0, c);
-
-    c = malloc(sizeof(int));
-    *c = 4;
-
-    ListInsert(&list, 3, c);
-
-    for (int i = 0; i < ListLenght(&list); i++)
-    {
-        int a = *((int *)(ListGetNode(&list, i)->data));
-        printf("%d\n", a);
-    }
-
+    ListFree(&list);
     return 0;
 }
